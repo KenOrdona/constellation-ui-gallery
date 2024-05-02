c46972a896bddc51585b7f71597180f7cb65042b
KenOrdona <64970196+KenOrdona@users.noreply.github.com>
2024-05-02 10:24:19 -0500

    Updated map utility

--- src/components/Pega_Extensions_MapUtility/index.tsx
+++ src/components/Pega_Extensions_MapUtility/index.tsx
@@ -2,22 +2,23 @@ import { useMemo } from 'react';
 import { LocationDisplay, MapsContext, Configuration } from '@pega/cosmos-react-core';
 
 type MapUtilityProps = {
-  incidentLocation: string;
+  location: string;
   getPConnect: any;
 };
 
 export default function PegaExtensionsMapUtility(props: MapUtilityProps) {
-  const { getPConnect, incidentLocation } = props;
+  const { getPConnect, location } = props;
 
   const mapApiKey: any = getPConnect().getGoogleMapsAPIKey();
 
   const ctx = useMemo(() => ({ name: 'google', apiKey: mapApiKey }), [mapApiKey]);
 
+  if (location === '') return null;
   return (
     <Configuration>
       <MapsContext.Provider value={ctx}>
         <LocationDisplay
-          value={incidentLocation}
+          value={location}
           zoomLevel={13}
           height='25rem'
           variant='map'
